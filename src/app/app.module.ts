13d196b8c54353bd3ea06aff9676d91777080b71
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-21 17:54:18 -0500

    add navbar to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NavbarComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

9da056f4e9ae85f34a48d9b03508f9e5df8c97da
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-21 18:16:32 -0500

    fix http return type

--- src/app/services/getCountr/get-all-endpoints.service.ts
+++ src/app/services/getCountr/get-all-endpoints.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AvailableCountriesInfo } from 'src/app/types';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +9,8 @@ export class GetAllEndpointsService {
   constructor(private http: HttpClient) {}
 
   getAvailableCountries() {
-    return this.http.get('https://date.nager.at/api/v3/AvailableCountries');
+    return this.http.get<AvailableCountriesInfo[]>(
+      'https://date.nager.at/api/v3/AvailableCountries'
+    );
   }
 }

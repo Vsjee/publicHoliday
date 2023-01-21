a328bd1fa2b61b8658749ee1347c7b531fdc7e2f
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-20 21:33:38 -0500

    add http to the service and new api

--- src/app/services/getCountr/get-all-endpoints.service.ts
+++ src/app/services/getCountr/get-all-endpoints.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GetAllEndpointsService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getAvailableCountries() {
+    return this.http.get('https://date.nager.at/api/v3/AvailableCountries');
+  }
 }

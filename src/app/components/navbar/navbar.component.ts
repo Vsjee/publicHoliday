e1ae6e5c22cbdae4fbb8027b63a74a8f5d9bab12
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-21 17:51:16 -0500

    add navbar component with material

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent {}

ff24700f46d5cca9b91e9248c3c802b18b2e6933
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-22 18:08:13 -0500

    fix re render when url param change

--- src/app/modules/holidays/holidays.module.ts
+++ src/app/modules/holidays/holidays.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HolidaysComponent } from './holidays.component';
 import { HolidaysRoutingModule } from './holiday-routing.module';
+import { BorderCountriesComponent } from './components/border-countries/border-countries.component';
 
 @NgModule({
   declarations: [HolidaysComponent],
-  imports: [CommonModule, HolidaysRoutingModule],
+  imports: [CommonModule, HolidaysRoutingModule, BorderCountriesComponent],
 })
 export class HolidaysModule {}

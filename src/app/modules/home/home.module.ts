2e1e5777167e4f98a943d57111c163a3bfd3ced7
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-22 12:08:17 -0500

    add virtual scroll to the home module

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from '../home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { VirtualScrollComponent } from 'src/app/components';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, VirtualScrollComponent],
 })
 export class HomeModule {}

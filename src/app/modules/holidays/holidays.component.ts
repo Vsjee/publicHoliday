8dc189d62c76c3aaea29a96bf3d5c171f5f2b0c3
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-22 12:05:35 -0500

    add get the url param

--- src/app/modules/holidays/holidays.component.ts
+++ src/app/modules/holidays/holidays.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { countrieInfoInitialized } from 'src/app/models';
+import { GetAllEndpointsService } from 'src/app/services';
+import { CountryInfo } from 'src/app/types';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-holidays',
   templateUrl: './holidays.component.html',
   styleUrls: ['./holidays.component.scss'],
 })
-export class HolidaysComponent {}
+export class HolidaysComponent {
+  countrieInfo: CountryInfo = countrieInfoInitialized;
+  countryCode: string | any = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private countryInfo: GetAllEndpointsService
+  ) {}
+
+  ngOnInit() {
+    this.countryCode = this.route.snapshot.paramMap.get('country');
+
+    this.countryInfo
+      .getCountrieHolidaysData(this.countryCode)
+      .subscribe((data) => {
+        this.countrieInfo = data;
+        console.log(this.countrieInfo);
+      });
+  }
+}

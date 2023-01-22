ff4d670bc740d81e09849c3aa8e5c34fe6bde0cd
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-22 11:48:16 -0500

    add country info types

--- src/app/types/endpoints.type.ts
+++ src/app/types/endpoints.type.ts
@@ -2,3 +2,19 @@ export interface AvailableCountriesInfo {
   countryCode: string;
   name: string;
 }
+
+export interface CountryInfo {
+  commonName: string;
+  officialName: string;
+  countryCode: string;
+  region: string;
+  borders: [
+    {
+      commonName: string;
+      officialName: string;
+      countryCode: string;
+      region: string;
+      borders: null;
+    }
+  ];
+}

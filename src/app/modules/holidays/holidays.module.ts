940596536e0a7abe9c3f096ba71992e52c39f20a
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-23 22:10:09 -0500

    add comp import to module

--- src/app/modules/holidays/holidays.module.ts
+++ src/app/modules/holidays/holidays.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HolidaysComponent } from './holidays.component';
 import { HolidaysRoutingModule } from './holiday-routing.module';
-import { BorderCountriesComponent } from './components/border-countries/border-countries.component';
+import {
+  BorderCountriesComponent,
+  HolidaysCountryDataComponent,
+} from './components';
 
 @NgModule({
   declarations: [HolidaysComponent],
-  imports: [CommonModule, HolidaysRoutingModule, BorderCountriesComponent],
+  imports: [
+    CommonModule,
+    HolidaysRoutingModule,
+    BorderCountriesComponent,
+    HolidaysCountryDataComponent,
+  ],
 })
 export class HolidaysModule {}

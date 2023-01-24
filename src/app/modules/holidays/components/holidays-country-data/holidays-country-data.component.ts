c8bb73dee119d580edfe3f1e3931dac7c34e7df5
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-24 11:03:45 -0500

    add common module

--- src/app/modules/holidays/components/holidays-country-data/holidays-country-data.component.ts
+++ src/app/modules/holidays/components/holidays-country-data/holidays-country-data.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { CountryHolidaysData } from 'src/app/types';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   selector: 'app-holidays-country-data',
   templateUrl: './holidays-country-data.component.html',
   styleUrls: ['./holidays-country-data.component.scss'],

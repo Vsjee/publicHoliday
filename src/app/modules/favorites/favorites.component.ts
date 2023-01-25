a5371a6734a74dc317759f1118d70581949cfe92
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-25 14:38:12 -0500

    add get data from the store

--- src/app/modules/favorites/favorites.component.ts
+++ src/app/modules/favorites/favorites.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { FavoriteInfo } from 'src/app/models';
+import { AppState } from 'src/app/state';
+import { getFavoriteItems } from 'src/app/state/favorites/favorites.selectors';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.scss']
+  styleUrls: ['./favorites.component.scss'],
 })
 export class FavoritesComponent {
+  favoriteItemsList: FavoriteInfo[] = [];
 
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.store.select(getFavoriteItems).subscribe((data) => {
+      this.favoriteItemsList = data;
+    });
+  }
 }

c815a69699aad709e1ccae89b7e60a5670187b71
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-24 21:47:34 -0500

    add logic for the search input

--- src/app/components/virtual-scroll/virtual-scroll.component.ts
+++ src/app/components/virtual-scroll/virtual-scroll.component.ts
@@ -1,27 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ScrollingModule } from '@angular/cdk/scrolling';
-import { AvailableCountriesInfo } from 'src/app/types';
-import { GetAllEndpointsService } from 'src/app/services';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { GetAllEndpointsService } from 'src/app/services';
+import { AvailableCountriesInfo } from 'src/app/types';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   standalone: true,
-  imports: [ScrollingModule, RouterModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    ScrollingModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   selector: 'app-virtual-scroll',
   templateUrl: './virtual-scroll.component.html',
   styleUrls: ['./virtual-scroll.component.scss'],
 })
 export class VirtualScrollComponent {
   availableCountries: AvailableCountriesInfo[] = [];
+  searchAvailableCountries: AvailableCountriesInfo[] = [];
+
+  form = new FormGroup({
+    search: new FormControl(''),
+  });
 
   constructor(private getData: GetAllEndpointsService) {}
+
+  handleSubmit(): void {
+    this.searchAvailableCountries = this.availableCountries.filter((item) => {
+      let searchValue: string | any = this.form.value.search;
+      let countryName: string = item.name.toLowerCase();
+      let match: string = searchValue.toLowerCase();
+      return countryName.includes(match);
+    });
+  }
+
+  handleChange(): void {
+    this.searchAvailableCountries = this.availableCountries;
+  }
 
   ngOnInit() {
     this.getData
       .getAvailableCountries()
       .subscribe((data: AvailableCountriesInfo[]) => {
         this.availableCountries = data;
+        this.searchAvailableCountries = data;
       });
   }
 }

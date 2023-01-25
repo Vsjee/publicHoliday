ad789abb23c092ef538912fdff89070853b98cee
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-25 14:41:01 -0500

    add imports

--- src/app/modules/favorites/favorites.module.ts
+++ src/app/modules/favorites/favorites.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FavoritesComponent } from './favorites.component';
-
-
+import { favoritesRoutingModule } from './favorites-routing.module';
+import { FavoriteButtonComponent } from 'src/app/components';
 
 @NgModule({
-  declarations: [
-    FavoritesComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [FavoritesComponent],
+  imports: [CommonModule, favoritesRoutingModule, FavoriteButtonComponent],
 })
-export class FavoritesModule { }
+export class FavoritesModule {}

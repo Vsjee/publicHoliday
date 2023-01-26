66a0b3a465db370f23529b1715aad4bad5d88517
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-26 11:41:04 -0500

    fix get local data for reusability

--- src/app/utilities/getLocalStorageData.ts
+++ src/app/utilities/getLocalStorageData.ts
@@ -1,9 +1,5 @@
-import { key } from '../components';
-import { FavoriteInfo } from '../models';
-
-export const getLocalStorageData = (): FavoriteInfo[] => {
+export const getLocalStorageData = (key: string) => {
   const getLocalData = localStorage.getItem(key);
-  const parseLocalData: FavoriteInfo[] =
-    getLocalData !== null ? JSON.parse(getLocalData) : [];
+  const parseLocalData = getLocalData !== null ? JSON.parse(getLocalData) : [];
   return parseLocalData;
 };
